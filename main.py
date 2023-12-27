9d2abc229c2fe44441d6429dd97b9116e6402b17
micasa-acerman <koha009@yandex.ru>
2023-12-27 17:52:54 +0300

    upd prompt logic

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 from pyrogram import enums
 from random import Random
 
+from gpt import gpt
 import string
 
 def preprocess_text(text):
@@ -16,7 +17,6 @@ def preprocess_text(text):
     
     return text_no_punctuation
 
-from gpt import gpt
 
 api_id = 12345
 api_hash = "0123456789abcdef0123456789abcdef"
@@ -30,9 +30,9 @@ app = Client("my_account", api_id=api_id, api_hash=api_hash)
 @app.on_message(filters.text & filters.private)
 async def echo(client, message):
     my_id = 6546956005
-    if message.from_user and (message.from_user.id == my_id or message.from_user.id == 1234060895):
+    if message.from_user and (message.from_user.id == my_id or message.from_user.id == 1234060895) or message.from_user.first_name == None:
         return
-    timeout = Random().randint(6,20)
+    timeout = Random().randint(2,5)
     print(f'Получено новое сообщение. Таймаут: {timeout}')
     time.sleep(timeout)
     crt_timestamp = None
@@ -41,30 +41,27 @@ async def echo(client, message):
         text = item.text
         if crt_timestamp == None:
             crt_timestamp = item.date.timestamp()
+            continue
         if text == None:
             if item.sticker != None:
                 text = item.sticker.emoji
             else:
                 text = "[Какая-то фотография]"
-        if item.from_user.id == my_id:
-            conversation.append({"role": "system", "content": f"{item.from_user.first_name}: {text}"})
-        else:
-            conversation.append({"role": "user", "content": f"{item.from_user.first_name}: {text}"})
+        conversation.append(f"{item.from_user.first_name}: {text}")
             
-    conversation = list(reversed(conversation))
+    conversation = "\r\n".join(list(reversed(conversation)))+f".\r\nMilana отвечает с большим юмором и любопытством. Он также может задать вопрос:"
     
     await app.read_chat_history(message.chat.id)
-    time.sleep(5)
     await app.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-    last_msg = [i async for i in app.get_chat_history(message.chat.id, limit=1)][0]
 
+    txt=gpt("Milana", message.from_user.first_name, conversation)
+    if txt.find(':') != -1:
+        txt = txt.split(':')[1]
+    print(f'Переключение в режим typing и ожидание {len(txt)//10+1} секунд')
+    await app.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
+    time.sleep(len(txt)//20+1)
+    last_msg = [i async for i in app.get_chat_history(message.chat.id, limit=1)][0]
     if crt_timestamp == None or crt_timestamp ==  last_msg.date.timestamp():
-        txt=gpt(conversation)
-        if txt.find(':') != -1:
-            txt = txt.split(':')[1]
-        print(f'Переключение в режим typing и ожидание {len(txt)//10+1} секунд')
-        await app.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-        time.sleep(len(txt)//10+1)
         await message.reply(preprocess_text(txt))
     else:
         print('skip')
