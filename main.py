1aafdbf094be8ae88cbebedbcd45531cdf94b061
micasa-acerman <koha009@yandex.ru>
2023-12-29 14:06:05 +0300

    upd prompt

--- main.py
+++ main.py
@@ -27,6 +27,14 @@ api_hash = '9501211cbdb6b33f3ebfcf92d1375336'
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
+def count_of_spaces(string):
+    count = 0
+    for i in range(0, len(string)):
+        if string[i] == " ":
+            count += 1
+    return count
+
+
 @app.on_message(filters.text & filters.private)
 async def echo(client, message):
     my_id = 6546956005
@@ -57,9 +65,10 @@ async def echo(client, message):
     txt=gpt("Milana", message.from_user.first_name, conversation)
     if txt.find(':') != -1:
         txt = txt.split(':')[1]
-    print(f'Переключение в режим typing и ожидание {len(txt)//10+1} секунд')
+    timeout = max(count_of_spaces(txt) // 5,1)
+    print(f'Переключение в режим typing и ожидание {timeout} секунд')
     await app.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-    time.sleep(len(txt)//20+1)
+    time.sleep(timeout)
     last_msg = [i async for i in app.get_chat_history(message.chat.id, limit=1)][0]
     if crt_timestamp == None or crt_timestamp ==  last_msg.date.timestamp():
         await message.reply(preprocess_text(txt))
